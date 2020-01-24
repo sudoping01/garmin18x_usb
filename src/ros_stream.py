cb7f481556ba38723f0e72899853595ab78b51ec
chriswsuarez <chriswsuarez@utexas.edu>
2020-01-24 14:43:49 -0600

    Renamed the original python script to terminal_stream and made a new script with associated launch and config files to publish a NavSatFix ROS msg.  Still in progress

---
+++ src/ros_stream.py
@@ -0,0 +1,70 @@
+#! /usr/bin/env python
+
+import sys, os, math
+import rospy
+from sensor_msgs.msg import NavSatFix# , NavSatStatus
+# from std_msgs.msg import Header
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.realpath(__file__))) + "/pygarmin")
+import garmin
+
+
+class GarminToRos:
+
+	def __init__(self):
+
+		# Create a 'physical layer' connection using serial port
+		phys = garmin.SerialLink("/dev/sensors/garmin_gps")
+
+		# Create a Garmin object using this connection
+		gps = garmin.Garmin(phys)
+
+		# Turn on Position Velocity Time
+		gps.pvtOn()
+
+		# Ros Parameters
+		self.pub_topic = rospy.get_param("garmin_gps/pub_topic", "/fix")
+		self.frame_id = rospy.get_param("garmin_gps/frame_id", "gps")
+		self.pub_rate = rospy.get_param("garmin_gps/pub_rate", 1.0)
+
+		# Publishers and subscribers
+		# rospy.Subscriber(self.twist_sub_topic, Twist, self.update_twist)
+		self._pub_fix = rospy.Publisher(self.pub_topic, NavSatFix, queue_size=1)
+
+		self.fix = NavSatFix()
+		rate = rospy.Rate(self.pub_rate)
+
+		while not rospy.is_shutdown():
+			data = gps.getPvt()
+			lat = data.rlat * 180 / math.pi
+			lon = data.rlon * 180 / math.pi
+
+			if abs(int(lat * 1e10) - int(self.fix.latitude * 1e10)) and abs(int(lon * 1e10) - int(self.fix.latitude * 1e10)) > 0 and abs(int(data.alt * 1e10) - int(self.fix.altitude * 1e10)):
+				self.fix.status.status = 1
+			else:
+				self.fix.status.status = 0
+
+			self.fix.header.stamp = rospy.get_rostime()
+			self.fix.header.frame_id = self.frame_id
+			self.fix.status.service = 1
+			self.fix.latitude = lat
+			self.fix.longitude = lon
+			self.fix.altitude = data.alt
+			self.fix.position_covariance = [0] * 9
+			self.fix.position_covariance_type = 0
+			# Unused gps data fields - fix, epe, eph, epv, tow, east, north, up, msl_height, leap_secs, wn_days
+
+			self._pub_fix.publish(self.fix)
+
+			# getPvt is called again and not used because apparently the data is in a different format when called an even number of times.  The next call will provide data I can currently understand.
+			gps.getPvt()
+			rate.sleep()
+
+if __name__=='__main__':
+	rospy.init_node('garmin_gps')
+
+	try:
+		_GarminToRos = GarminToRos()
+
+	except rospy.ROSInterruptException: 
+		pass
