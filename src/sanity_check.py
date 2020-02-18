e88aed0dfdfad472fae2ef2905e9d314c2ac5b3e
PaperFanz <petefan.7@gmail.com>
2020-02-18 12:10:46 -0600

    gps_base and CMake fixes

--- src/sanity_check.py
+++ src/sanity_check.py
@@ -21,22 +21,22 @@ class GpsCorrectionTest:
 		self.rlat1 = self.base_lat * pi / 180
 		self.rlon1 = self.base_lon * pi / 180
 
-		self.rover_odom = BaseToRover()
+		self.rover = BaseToRover()
 
 		while not rospy.is_shutdown():
 			pass
 
 	def update_data(self, fix_in):
-		if self.fix.status.status == 1:
+		if fix_in.status.status == 1:
 			rlat2 = fix_in.latitude * pi / 180
 			rlon2 = fix_in.longitude * pi / 180
 			dlat =  rlat2 - self.rlat1
-			dlon =  rlon2 - self.rlon2
-			a = sin(dlat / 2)**2 + cos(lat1) * cos(lat2) * sin(dlon/2)**2
+			dlon =  rlon2 - self.rlon1
+			a = sin(dlat / 2)**2 + cos(self.rlat1) * cos(rlat2) * sin(dlon/2)**2
 			c = 2 * asin(sqrt(a))
 			self.rover.distance = c * 6371
-			self.rover.bearing = atan2( sin(lon2 - self.lon1) * cos(lat2), cos(self.lat1) * sin(lat2) - sin(self.lat1) * cos(lat2) * cos(lon2 - self.lon1))
-			self.rover.rel_alt = fix_in.alt - self.base_alt
+			self.rover.bearing = atan2( sin(rlon2 - self.rlon1) * cos(rlat2), cos(self.rlat1) * sin(rlat2) - sin(self.rlat1) * cos(rlat2) * cos(rlon2 - self.rlon1))
+			self.rover.rel_alt = fix_in.altitude - self.base_alt
 			# Unused gps data fields - fix, epe, eph, epv, tow, east, north, up, msl_height, leap_secs, wn_days
 
 			# getPvt is called again and not used because apparently the data is in a different format when called an even number of times.  The next call will provide data I can currently understand.
