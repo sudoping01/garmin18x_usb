530f4d824266678469c5f821620eb3b38e54298d
sudoping01 <sudoping01@gmail.com>
2024-08-01 23:42:15 +0000

    update according to the new version of pygarmin

--- src/gps_correction_rover.py
+++ src/gps_correction_rover.py
@@ -1,73 +1,85 @@
 #!/usr/bin/env python
 
-import sys, os, math
+import sys
+import os
+import math
 import rospy
 from garmin18x_usb.msg import GpsCorrection
 from sensor_msgs.msg import NavSatFix
 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.realpath(__file__))) + "/pygarmin")
-import garmin
+from pygarmin import garmin, link
 
 class GpsRover:
 
-	def __init__(self):
-		self.correction_sub_topic = rospy.get_param("gps_correction_rover/correction_sub_topic", "fix/correction")	
-		self.pub_topic = rospy.get_param("gps_correction_rover/pub_topic", "fix")
-		self.pub_rate = rospy.get_param("gps_correction_rover/pub_rate", 1.0)
-		dev_path = rospy.get_param("gps_correction_rover/dev_path", "/dev/sensors/garmin_gps")
+    def __init__(self):
+        self.correction_sub_topic = rospy.get_param("gps_correction_rover/correction_sub_topic", "fix/correction")
+        self.pub_topic = rospy.get_param("gps_correction_rover/pub_topic", "fix")
+        self.pub_rate = rospy.get_param("gps_correction_rover/pub_rate", 1.0)
+        dev_path = rospy.get_param("gps_correction_rover/dev_path", "/dev/sensors/garmin_gps")
 
-		# Create a 'physical layer' connection using serial port
-		phys = garmin.SerialLink(dev_path)
+        # Create a 'physical layer' connection using serial port
+        phys = link.SerialLink(dev_path)
 
-		# Create a Garmin object using this connection
-		self.gps = garmin.Garmin(phys)
-		
-		# Turn on Position Velocity Time
-		self.gps.pvtOn()
+        # Create a Garmin object using this connection
+        self.gps = garmin.Garmin(phys)
+        
+        # Turn on Position Velocity Time
+        self.gps.pvt_on()
 
-		rospy.Subscriber(self.correction_sub_topic, GpsCorrection, self.correct_fix)
-		self._pub_fix = rospy.Publisher(self.pub_topic, NavSatFix, queue_size=1)
-		self.fix = NavSatFix()
+        rospy.Subscriber(self.correction_sub_topic, GpsCorrection, self.correct_fix)
+        self._pub_fix = rospy.Publisher(self.pub_topic, NavSatFix, queue_size=1)
+        self.fix = NavSatFix()
 
-		rate = rospy.Rate(self.pub_rate)
-		
-		while not rospy.is_shutdown():
-			pass
+        rate = rospy.Rate(self.pub_rate)
+        
+        while not rospy.is_shutdown():
+            rate.sleep()
 
-	def correct_fix(self, correction):
-		data = self.gps.getPvt()
-		
-		if int(data.tow) == int(correction.tow):
-			lat = data.rlat * 180 / math.pi
-			lon = data.rlon * 180 / math.pi
+    def correct_fix(self, correction):
+        data = self.gps.get_pvt()
+        
+        if int(data.tow) == int(correction.tow):
+            posn = getattr(data, 'posn', [None, None])
+            lat = posn[0]
+            lon = posn[1]
+            
+            if lat is not None and lon is not None:
+                lat_deg = lat * 180 / math.pi
+                lon_deg = lon * 180 / math.pi
 
-			if abs(int(lat * 1e10) - int(self.fix.latitude * 1e10)) > 0 and abs(int(lon * 1e10) - int(self.fix.latitude * 1e10)) > 0 and abs(int(data.alt * 1e10) - int(self.fix.altitude * 1e10)) > 0:
-				self.fix.status.status = 1
-			else:
-				self.fix.status.status = 0
+                # Check if the current fix is different from the last published fix
+                if abs(int(lat_deg * 1e10) - int(self.fix.latitude * 1e10)) > 0 and \
+                   abs(int(lon_deg * 1e10) - int(self.fix.longitude * 1e10)) > 0 and \
+                   abs(int(getattr(data, 'alt', 0) * 1e10) - int(self.fix.altitude * 1e10)) > 0:
+                    self.fix.status.status = 1  # Status: 1 indicates that the GPS fix is valid
+                else:
+                    self.fix.status.status = 0  # Status: 0 indicates that the GPS fix is not valid
 
-			self.fix.header.stamp = rospy.get_rostime()
-			self.fix.header.frame_id = "gps"
-			self.fix.status.service = 1
-			self.fix.latitude = lat + correction.lat
-			self.fix.longitude = lon + correction.lon
-			self.fix.altitude = data.alt + correction.alt
-			self.fix.position_covariance = [0] * 9
-			self.fix.position_covariance_type = 0
-			self._pub_fix.publish(self.fix)
+                self.fix.header.stamp = rospy.get_rostime()
+                self.fix.header.frame_id = "gps"
+                self.fix.status.service = 1
+                self.fix.latitude = lat_deg + correction.lat
+                self.fix.longitude = lon_deg + correction.lon
+                self.fix.altitude = getattr(data, 'alt', 0) + correction.alt
+                self.fix.position_covariance = [0] * 9
+                self.fix.position_covariance_type = 0
+                self._pub_fix.publish(self.fix)
 
-		else:
-			rospy.logerr("GPS Correction received but the Time of Week was not the same as the on board GPS! Not publishing a corrected gps fix.")
-			rospy.logerr("TOW Onboard: " + str(data.tow))
-			rospy.logerr("TOW Correction: "+ str(correction.tow))
+            else:
+                rospy.logerr("Latitude or Longitude is None.")
+        else:
+            rospy.logerr("GPS Correction received but the Time of Week was not the same as the onboard GPS! Not publishing a corrected GPS fix.")
+            rospy.logerr(f"TOW Onboard: {data.tow}")
+            rospy.logerr(f"TOW Correction: {correction.tow}")
 
-		# getPvt is called again and not used because apparently the data is in a different format when called an even number of times.  The next call will provide data I can currently understand.
-		self.gps.getPvt()
+        # get_pvt is called again and not used because the data is in a different format when called an even number of times. 
+        self.gps.get_pvt()
 
-if __name__=='__main__':
-	rospy.init_node('gps_correction_rover')
+if __name__ == '__main__':
+    rospy.init_node('gps_correction_rover')
 
-	try:
-		_GpsRover = GpsRover()
-	except rospy.ROSInterruptException: 
-		pass
+    try:
+        GpsRover()
+    except rospy.ROSInterruptException:
+        pass
--- src/ros_stream.py
+++ src/ros_stream.py
@@ -1,69 +1,86 @@
-#! /usr/bin/env python
+#!/usr/bin/env python
 
-import sys, os, math
+import sys
+import os
+import math
 import rospy
 from sensor_msgs.msg import NavSatFix
 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.realpath(__file__))) + "/pygarmin")
-import garmin
-
+from pygarmin import garmin, link
 
 class GarminToRos:
 
-	def __init__(self):
+    def __init__(self):
 
-		# Ros Parameters
-		self.pub_topic = rospy.get_param("garmin_gps/pub_topic", "/fix")
-		self.frame_id = rospy.get_param("garmin_gps/frame_id", "gps")
-		self.pub_rate = rospy.get_param("garmin_gps/pub_rate", 1.0)
-		dev_path = rospy.get_param("garmin_gps/dev_path", "/dev/sensors/garmin_gps")
+        # Ros Parameters
+        self.pub_topic = rospy.get_param("garmin_gps/pub_topic", "/fix")
+        self.frame_id = rospy.get_param("garmin_gps/frame_id", "gps")
+        self.pub_rate = rospy.get_param("garmin_gps/pub_rate", 1.0)
+        dev_path = rospy.get_param("garmin_gps/dev_path", "/dev/sensors/garmin_gps")
 
-		# Create a 'physical layer' connection using serial port
-		phys = garmin.SerialLink(dev_path)
+        # Create a 'physical layer' connection using serial port
+        phys = link.SerialLink(dev_path)
 
-		# Create a Garmin object using this connection
-		gps = garmin.Garmin(phys)
-		
-		# Turn on Position Velocity Time
-		gps.pvtOn()
+        # Create a Garmin object using this connection
+        gps = garmin.Garmin(phys)
+        
+        # Turn on Position Velocity Time
+        gps.pvt_on()
 
-		# Publishers and subscribers
-		self._pub_fix = rospy.Publisher(self.pub_topic, NavSatFix, queue_size=1)
+        # Publisher for NavSatFix messages
+        self._pub_fix = rospy.Publisher(self.pub_topic, NavSatFix, queue_size=1)
 
-		self.fix = NavSatFix()
-		rate = rospy.Rate(self.pub_rate)
+        # Initialize NavSatFix message
+        self.fix = NavSatFix()
 
-		while not rospy.is_shutdown():
-			data = gps.getPvt()
-			lat = data.rlat * 180 / math.pi
-			lon = data.rlon * 180 / math.pi
+        rate = rospy.Rate(self.pub_rate)
 
-			if abs(int(lat * 1e10) - int(self.fix.latitude * 1e10)) > 0 and abs(int(lon * 1e10) - int(self.fix.latitude * 1e10)) > 0 and abs(int(data.alt * 1e10) - int(self.fix.altitude * 1e10)) > 0:
-				self.fix.status.status = 1
-			else:
-				self.fix.status.status = 0
+        while not rospy.is_shutdown():
+            try:
+                data = gps.get_pvt()
 
-			self.fix.header.stamp = rospy.get_rostime()
-			self.fix.header.frame_id = self.frame_id
-			self.fix.status.service = 1
-			self.fix.latitude = lat
-			self.fix.longitude = lon
-			self.fix.altitude = data.alt
-			self.fix.position_covariance = [0] * 9
-			self.fix.position_covariance_type = 0
-			# Unused gps data fields - fix, epe, eph, epv, tow, east, north, up, msl_height, leap_secs, wn_days
+                # Extract latitude and longitude in degrees
+                posn = getattr(data, 'posn', [None, None])
+                lat = posn[0]
+                lon = posn[1]
 
-			self._pub_fix.publish(self.fix)
+                if lat is not None and lon is not None:
+                    lat_deg = lat * 180 / math.pi
+                    lon_deg = lon * 180 / math.pi
 
-			# getPvt is called again and not used because apparently the data is in a different format when called an even number of times.  The next call will provide data I can currently understand.
-			gps.getPvt()
-			rate.sleep()
+                    # Check if there is a significant change in position
+                    if (abs(int(lat_deg * 1e10) - int(self.fix.latitude * 1e10)) > 0 or 
+                        abs(int(lon_deg * 1e10) - int(self.fix.longitude * 1e10)) > 0 or
+                        abs(int(getattr(data, 'alt', 0) * 1e10) - int(self.fix.altitude * 1e10)) > 0):
+                        self.fix.status.status = 1  # Status: 1 indicates that the GPS fix is valid
+                    else:
+                        self.fix.status.status = 0  # Status: 0 indicates that the GPS fix is not valid
 
-if __name__=='__main__':
-	rospy.init_node('garmin_gps')
+                    # Update NavSatFix message
+                    self.fix.header.stamp = rospy.get_rostime()
+                    self.fix.header.frame_id = self.frame_id
+                    self.fix.status.service = 1
+                    self.fix.latitude = lat_deg
+                    self.fix.longitude = lon_deg
+                    self.fix.altitude = getattr(data, 'alt', 0)
+                    self.fix.position_covariance = [0] * 9
+                    self.fix.position_covariance_type = 0
 
-	try:
-		_GarminToRos = GarminToRos()
+                    # Publish the updated GPS fix
+                    self._pub_fix.publish(self.fix)
+                else:
+                    rospy.logerr("Latitude or Longitude is None.")
 
-	except rospy.ROSInterruptException: 
-		pass
+            except Exception as e:
+                rospy.logerr(f"Unexpected error: {e}")
+
+            rate.sleep()
+
+if __name__ == '__main__':
+    rospy.init_node('garmin_gps')
+
+    try:
+        GarminToRos()
+    except rospy.ROSInterruptException:
+        pass
